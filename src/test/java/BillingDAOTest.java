1e90130a3f6d2a6496ac183c153ef50da878bb30
Hamza Younes <145718803+Hamza-005@users.noreply.github.com>
2025-05-19 02:15:05 +0300

    billing test

--- src/test/java/BillingDAOTest.java
+++ src/test/java/BillingDAOTest.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class billingdaotestjunit {
+public class BillingDAOTest {
     private static Connection conn;
     private static BillingDAO dao;
     private static final String DB_URL = "jdbc:mysql://localhost:3308/SpecialCookDB";
